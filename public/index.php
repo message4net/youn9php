0128826c0051a24e004c3b2c0092d7c91b6f8594
范永迪 <message4net@outlook.com>
2017-12-14 01:24:26 +0800

    modified:   public/index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php session_start();
 
 //定义起始URL
-define('URI_BASE', '/index.php');
-define('URL_REL', 'http://127.0.0.1/youn9php/');
+//define('URI_BASE', '/index.php');
+//define('URL_REL', 'http://127.0.0.1/youn9php/');
+
+define('URI_BASE', '/public/index.php');
+define('URL_REL', 'http://127.0.0.1:8888/youn9php/');
+
 
 require __DIR__.'/../index.php';
